bc2453ac2003fe50d80cceacb7fe4c13bc131bda
Marwen Abid <marwen.abid@stellar.org>
2025-10-24 09:48:50 -0700

    add gh actions for pr preview

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { fileURLToPath } from "node:url";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: process.env.VITE_BASE_PATH || "/",
   server: {
     port: 3000,
   },
